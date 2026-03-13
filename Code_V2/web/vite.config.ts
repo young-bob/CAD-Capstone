2bf0d4f349317a012b95783ddd885e2e52320c6c
Chunxi Zhang <Czhang1818@conestogac.on.ca>
2026-03-13 15:52:49 -0400

    Recover vite setting

--- Code_V2/web/vite.config.ts
+++ Code_V2/web/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         open: true,
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'http://10.20.30.1',
                 changeOrigin: true,
             },
         },
